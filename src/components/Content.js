f1231292034b4526cff12c522507d7a5113e581f
alexandral1221 <alexandra_lakatos15@yahoo.com>
2020-02-04 22:19:51 +0200

    Add the redirect to the page with all categories

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Page from "./Page";
+import {Redirect, Route, Switch} from "react-router";
 
 class Content extends React.Component {
     render() {
         return (
             <div className="ui container">
+                <Switch>
+                    <Route exact path="/"><Redirect to="/categorii"/></Route>
+                </Switch>
                 <Page/>
             </div>
         )
