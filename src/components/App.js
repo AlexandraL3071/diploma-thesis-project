89a373c1d7c8ae65b0bc17abb27bbe4bdad7a8ea
alexandral1221 <alexandra_lakatos15@yahoo.com>
2020-02-18 21:10:39 +0200

    Restore the header

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,10 @@ import Header from './Header';
 import Content from './Content';
 import {BrowserRouter as Router} from 'react-router-dom';
 import history from '../history'
-import '../styles/Header.css'
 
 function App() {
     return (
-        <div id="overflow">
+        <div>
             <Router history={history}>
                     <Header title='E-com'/>
                     <Content/>
