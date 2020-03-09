435aa53293dddabc160d795504d7bbcbe65de051
alexandral1221 <alexandra_lakatos15@yahoo.com>
2020-03-09 09:03:11 +0200

    Add to git the files which makes the offline functionality

---
+++ src/components/PagingComponent.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react'
+import {IonButton, IonContent, IonItem, IonLabel} from "@ionic/react";
+import '../styles/PagingComponent.css'
+
+export default function PagingComponent(props) {
+    const [pageNumber, setPageNumber] = useState(0);
+
+    const maxPageNumber = () => {
+        return ((props.length)/5 + 1) | 0;
+    };
+
+    const goBack = () => {
+        props.parentCallback(pageNumber - 1);
+        setPageNumber(pageNumber - 1);
+    };
+
+    const goForward = () => {
+        props.parentCallback(pageNumber + 1);
+        setPageNumber(pageNumber + 1);
+    };
+
+    return (
+        <IonContent id="full-width">
+            <IonItem id="page-item">
+                {pageNumber > 0 ? <IonButton className="paging-button" color="dark"
+                                             onClick={() => goBack()} >&lt;</IonButton> : ""}
+                <IonLabel id="page-label">{pageNumber + 1} din {maxPageNumber()}</IonLabel>
+                {pageNumber < maxPageNumber() - 1 ? <IonButton className="paging-button" color="dark"
+                                                         onClick={() => goForward()}>&gt;</IonButton> : ""}
+            </IonItem>
+        </IonContent>
+    )
+}
---
+++ src/components/ProductsPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {IonContent, IonHeader} from "@ionic/react";
+import ProductCard from "./categories/ProductCard";
+import {heartOutline} from "ionicons/icons";
+import {FAVORITE_LINK} from "../utils/linkNames";
+import PagingComponent from "./PagingComponent";
+
+export default function ProductsPage(props) {
+    return (
+        <IonContent>
+            {props.products.map(product => (
+                <ProductCard product={product} type='add' icon={heartOutline}
+                             text='Adauga la favorite' link={FAVORITE_LINK}/>
+            ))}
+            {props.products.length === 0 ? <IonHeader>Sunteti in modul offline! Restul produselor vor fi afisate cand reveniti online.</IonHeader> : ''}
+            <PagingComponent length={props.length} parentCallback={props.parentCallback}/>
+        </IonContent>
+    )
+}
